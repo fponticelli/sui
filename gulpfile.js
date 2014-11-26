dd58391135c230b590f6de4a18a5aedd32bca003
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-11-26 09:25:14 -0700

    attempt at embedding icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-var gulp = require('gulp'),
-    stylus = require('gulp-stylus'),
-    connect = require('gulp-connect'),
-    nib = require('nib');;
+var gulp      = require('gulp'),
+    seq       = require('gulp-run-sequence'),
+    stylus    = require('gulp-stylus'),
+    connect   = require('gulp-connect'),
+    nib       = require('nib'),
+    svgSprite = require("gulp-svg-sprites"),
+    execSync  = require('exec-sync'),
+    Iconizr   = require('iconizr'),
+    tmp       = "assets/tmp";
 
 gulp.task('connect', function () {
   connect.server({
@@ -21,8 +26,34 @@ gulp.task('js', function () {
     .pipe(connect.reload());
 });
 
-gulp.task('stylus', function () {
-  gulp.src('./style/*.styl')
+gulp.task('icons', function (cb) {
+  var options = {
+      render : {
+        css : false,
+        styl : {
+          dest : '../../style/icons.styl'
+        }
+      },
+      prefix : 'sui-icon',
+      spritedir : '.',
+      quantize : true,
+      level : 5,
+      keep : false,
+    },
+    callback = function(err, results) {
+      if(err) throw "Failed to convert icons: " + err;
+      execSync("rm -rf " + tmp);
+      execSync("rm style/icons-png-*.*");
+      execSync("rm style/icons-*-sprite.*");
+      execSync("mv style/icons-svg-data.styl style/icons.styl");
+      cb();
+    };
+
+Iconizr.createIconKit('assets/icons', tmp, options, callback);
+});
+
+gulp.task('stylus', function() {
+  gulp.src('./style/sui.styl')
     .pipe(stylus({
       use: [nib()],
       compress: true
@@ -32,9 +63,12 @@ gulp.task('stylus', function () {
 });
 
 gulp.task('watch', function () {
+  gulp.watch(['./assets/icons/*.svg'],  ['icons']);
   gulp.watch(['./bin/*.html'], ['html']);
   gulp.watch(['./bin/*.js'], ['js']);
   gulp.watch(['./style/*.styl'], ['stylus']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', function (cb) {
+  seq(['connect', 'icons'], ['stylus', 'watch'], cb);
+});
