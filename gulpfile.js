74aeec4f9101407a4f87c60976e7ddb36c9810f9
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-11-27 15:05:09 -0700

    improved build

--- gulpfile.js
+++ gulpfile.js
@@ -28,28 +28,37 @@ gulp.task('js', function () {
 
 gulp.task('icons', function (cb) {
   var options = {
-      render : {
-        css : false,
-        styl : {
-          dest : '../../style/icons.styl'
-        }
+        render : {
+          css : false,
+          styl : {
+            dest : '../../style/icons.styl',
+            template : 'assets/templates/icons.styl'
+          }
+        },
+        prefix : 'sui-icon',
+        spritedir : '.',
+        quantize : true,
+        level : 5,
+        keep : false,
       },
-      prefix : 'sui-icon',
-      spritedir : '.',
-      quantize : true,
-      level : 5,
-      keep : false,
-    },
-    callback = function(err, results) {
-      if(err) throw "Failed to convert icons: " + err;
-      execSync("rm -rf " + tmp);
-      execSync("rm style/icons-png-*.*");
-      execSync("rm style/icons-*-sprite.*");
-      execSync("mv style/icons-svg-data.styl style/icons.styl");
-      cb();
-    };
+      callback = function(err, results) {
+        if(err) throw "Failed to convert icons: " + err;
+        execSync("rm -rf " + tmp);
+        execSync("rm style/icons-png-*.*");
+        execSync("rm style/icons-*-sprite.*");
+        execSync("mv style/icons-svg-data.styl style/icons.styl");
+        cb();
+      };
 
-Iconizr.createIconKit('assets/icons', tmp, options, callback);
+  Iconizr.createIconKit('assets/icons', tmp, options, callback);
+});
+
+gulp.task('build', function(cb) {
+  seq("stylus", "haxe", cb);
+});
+
+gulp.task('haxe', function() {
+  execSync("haxe build.hxml");
 });
 
 gulp.task('stylus', function() {
@@ -58,17 +67,16 @@ gulp.task('stylus', function() {
       use: [nib()],
       compress: true
     }))
-    .pipe(gulp.dest('./css'))
-    .pipe(connect.reload());
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./assets/icons/*.svg'],  ['icons']);
+  gulp.watch(['./assets/icons/*.svg'], function(cb) { seq('icons', 'build', cb); });
   gulp.watch(['./bin/*.html'], ['html']);
-  gulp.watch(['./bin/*.js'], ['js']);
-  gulp.watch(['./style/*.styl'], ['stylus']);
+  gulp.watch(['./bin/controls.js'], ['js']);
+  gulp.watch(['./style/*.styl'], ['build']);
 });
 
 gulp.task('default', function (cb) {
-  seq(['connect', 'icons'], ['stylus', 'watch'], cb);
+  seq(['connect', 'icons'], ['build', 'watch'], cb);
 });
