2e5dcfd68e1eb2c732c5c27675951c02d6d45df8
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-11-28 18:58:36 -0700

    separated control creation from Sui

--- src/sui/Sui.hx
+++ src/sui/Sui.hx
@@ -25,30 +25,57 @@ class Sui {
   }
 
   public function array<T>(?label : String, ?defaultValue : Array<T>, ?defaultElementValue : T, createControl : T -> IControl<T>, ?options : Options, callback : Array<T> -> Void)
-    return control(label, new ArrayControl((defaultValue).or([]), defaultElementValue, createControl, options), callback);
+    return control(label, createArray(defaultValue, defaultElementValue, createControl, options), callback);
 
   public function bool(?label : String, ?defaultValue = false, ?options : Options, callback : Bool -> Void)
-    return control(label, new BoolControl(defaultValue, options), callback);
+    return control(label, createBool(defaultValue, options), callback);
 
-  public function date(?label : String, ?defaultValue : Date, ?options : OptionsKindDate, callback : Date -> Void) {
+  public function color(?label : String, ?defaultValue = "#AA0000", ?options : OptionsColor, callback : String -> Void)
+    return control(label, createColor(defaultValue, options), callback);
+
+  public function date(?label : String, ?defaultValue : Date, ?options : OptionsKindDate, callback : Date -> Void)
+    return control(label, createDate(defaultValue, options), callback);
+
+  public function float(?label : String, ?defaultValue = 0.0, ?options : OptionsKindFloat, callback : Float -> Void)
+    return control(label, createFloat(defaultValue, options), callback);
+
+  public function int(?label : String, ?defaultValue = 0, ?options : OptionsKindInt, callback : Int -> Void)
+    return control(label, createInt(defaultValue, options), callback);
+
+  public function label(?defaultValue = "", ?label : String, ?callback : String -> Void)
+    return control(label, createLabel(defaultValue), callback);
+
+  public function text(?label : String, ?defaultValue = "", ?options : OptionsKindText, callback : String -> Void)
+    return control(label, createText(defaultValue, options), callback);
+
+  public function trigger(actionLabel : String, ?label : String, ?options : Options, callback : Void -> Void)
+    return control(label, new TriggerControl(actionLabel, options), function(_) callback());
+
+  // statics
+  static public function createArray<T>(?defaultValue : Array<T>, ?defaultElementValue : T, createControl : T -> IControl<T>, ?options : Options)
+    return new ArrayControl((defaultValue).or([]), defaultElementValue, createControl, options);
+
+  static public function createBool(?defaultValue = false, ?options : Options)
+    return new BoolControl(defaultValue, options);
+
+  static public function createColor(?defaultValue = "#AA0000", ?options : OptionsColor)
+    return new ColorControl(defaultValue, options);
+
+  static public function createDate(?defaultValue : Date, ?options : OptionsKindDate) {
     if(null == defaultValue)
       defaultValue = Date.now();
-    var ctrl = switch [(options.listonly).or(false), (options.kind).or(null)] {
+    return switch [(options.listonly).or(false), (options.kind).or(null)] {
       case [true, _]:
         new DateSelectControl(defaultValue, options);
       case [_, DateTime]:
         new DateTimeControl(defaultValue, options);
       case _:
         new DateControl(defaultValue, options);
-    }
-    return control(label, ctrl, callback);
+    };
   }
 
-  public function color(?label : String, ?defaultValue = "#AA0000", ?options : OptionsColor, callback : String -> Void)
-    return control(label, new ColorControl(defaultValue, options), callback);
-
-  public function float(?label : String, ?defaultValue = 0.0, ?options : OptionsKindFloat, callback : Float -> Void) {
-    var ctrl = switch [(options.listonly).or(false), (options.kind).or(null)] {
+  static public function createFloat(?defaultValue = 0.0, ?options : OptionsKindFloat)
+    return switch [(options.listonly).or(false), (options.kind).or(null)] {
       case [true, _]:
         new NumberSelectControl<Float>(defaultValue, options);
       case [_, FloatTime]:
@@ -58,23 +85,19 @@ class Sui {
         new FloatRangeControl(defaultValue, options) :
         new FloatControl(defaultValue, options);
     };
-    return control(label, ctrl, callback);
-  }
 
-  public function int(?label : String, ?defaultValue = 0, ?options : OptionsKindInt, callback : Int -> Void) {
-    var ctrl = (options.listonly).or(false) ?
+  static public function createInt(?defaultValue = 0, ?options : OptionsKindInt)
+    return (options.listonly).or(false) ?
           new NumberSelectControl<Int>(defaultValue, options) :
           (null != options && options.min != null && options.max != null) ?
             new IntRangeControl(defaultValue, options) :
             new IntControl(defaultValue, options);
-    return control(label, ctrl, callback);
-  }
 
-  public function label(?defaultValue = "", ?label : String, ?callback : String -> Void)
-    return control(label, new LabelControl(defaultValue), callback);
+  static public function createLabel(?defaultValue = "", ?label : String, ?callback : String -> Void)
+    return new LabelControl(defaultValue);
 
-  public function text(?label : String, ?defaultValue = "", ?options : OptionsKindText, callback : String -> Void) {
-    var ctrl = switch [(options.listonly).or(false), (options.kind).or(null)] {
+  static public function createText(?defaultValue = "", ?options : OptionsKindText)
+    return switch [(options.listonly).or(false), (options.kind).or(null)] {
       case [true, _]:         new TextSelectControl(defaultValue, options);
       case [_, TextEmail]:    new EmailControl(defaultValue, options);
       case [_, TextPassword]: new PasswordControl(defaultValue, options);
@@ -83,11 +106,9 @@ class Sui {
       case [_, TextUrl]:      new UrlControl(defaultValue, options);
       case [_, _]:            new TextControl(defaultValue, options);
     };
-    return control(label, ctrl, callback);
-  }
 
-  public function trigger(actionLabel : String, ?label : String, ?options : Options, callback : Void -> Void)
-    return control(label, new TriggerControl(actionLabel, options), function(_) callback());
+  static public function createTrigger(actionLabel : String, ?options : Options)
+    return new TriggerControl(actionLabel, options);
 
   // generic binding
   public function control<T, TControl : IControl<T>>(?label : String, control : TControl, callback : T -> Void) : TControl {
