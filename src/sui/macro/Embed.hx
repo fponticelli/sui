937022d265e8aa4303de4f36b0bdc62b316975d8
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-11-29 16:30:38 -0700

    embedding from lib and not current project

--- src/sui/macro/Embed.hx
+++ src/sui/macro/Embed.hx
@@ -1,10 +1,12 @@
 package sui.macro;
 
 import haxe.macro.Context;
+import thx.macro.Macros;
 
 class Embed {
   macro public static function file(path : String) {
-    path = Sys.getCwd() + path;
+    var suiPath = Macros.getModulePath("sui.Sui").split("/").slice(0, -2).join("/")+"/../";
+    path = suiPath + path;
     Context.registerModuleDependency("sui.Sui", path);
     var content = sys.io.File.getContent(path);
     return macro $v{content};
