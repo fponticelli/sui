e9c9db414d17f233bef314387ecb3d21f50913e2
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-11-29 16:33:21 -0700

    release

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 rm -f sui.zip
-zip -r sui.zip src test extraParams.hxml haxelib.json LICENSE README.md
+zip -r sui.zip src test css/sui.css extraParams.hxml haxelib.json LICENSE README.md
 haxelib submit sui.zip
