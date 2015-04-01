4c5cf3b25b1f5d92868401b04c1db3e7f6729410
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:10:46 -0600

    exclude hidden files from zip

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 rm -f sui.zip
-zip -r sui.zip src test css/sui.css extraParams.hxml haxelib.json LICENSE README.md
-haxelib submit sui.zip
+zip -r sui.zip src test css/sui.css extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
+haxelib submit sui.zip -x
